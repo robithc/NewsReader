890578acb1e41945abf3739db59e92f32d7bc2cb
RO221D <robithc@gmail.com>
2018-06-14 09:55:33 +0300

    added settings menu (for now its empty)

--- app/src/main/java/com/example/android/newsreader/NewsActivity.java
+++ app/src/main/java/com/example/android/newsreader/NewsActivity.java
@@ -11,6 +11,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -96,4 +98,25 @@ public class NewsActivity extends AppCompatActivity
     public void onLoaderReset(Loader loader) {
         mAdapter.clear();
     }
+
+
+    @Override
+    // This method initialize the contents of the Activity's options menu.
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the Options Menu we specified in XML
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
